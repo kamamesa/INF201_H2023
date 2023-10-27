613069117e861a0c7424b43631f3e2b838c92b29
kamamesa <111873167+kamamesa@users.noreply.github.com>
2023-10-27 14:33:51 +0200

    Update exercise_6.py

--- exercise_6.py
+++ exercise_6.py
@@ -11,17 +11,17 @@ Project task "Neural Network"
 # Import
 import numpy as np
 
-# For the stolen code
+# Required for read.py fragment
 from torchvision import datasets, transforms
 
 # Vector of dimensions for each layer
 n_vector = [784,512,256,10]
 
-# Files to read
+# List of files to read
 weights = ["W_"+str(number)+".txt" for number in range(1,len(n_vector))]
 biases = ["b_"+str(number)+".txt" for number in range(1,len(n_vector))]
 
-# ---- FUNCTIONS ----
+# ---- STANDALONE FUNCTIONS ----
 
 # Reads text files into lists of values
 def freader(wfile,bfile):
@@ -43,7 +43,7 @@ class layer():
         self.weight = np.ones((n,m))
         self.bias = np.ones(n)
 
-    # Fill in data from specified files
+    # Fill in data from specified weight/bias files.
     def read_file(self,w_f,b_f):
         u = freader(w_f,b_f)
         self.weight = u[0]
@@ -74,17 +74,16 @@ class network():
             self.layers[i].read_file(w[i],b[i])
 
     # Runs each layer's evaluate function
-    def run(self):
-        for i in range(len(self.layers)):
-            self.layers[i].evaluate()
+    def run(self,initial_y):
+        for i in range(len(self.layers)-1):
+#            self.layers[i].evaluate()
+            print(f"{len(self.layers)}")
+            return 1
+            
+#            ys.append(layer(Ws,ys,b))
 
+# ---- BLOCK FROM READ.PY ----
 
-# ---- BODY ----
-the_network = network(n_vector)
-the_network.fill(weights,biases)
-
-
-# Block nabbed from read.py
 def get_mnist():
     return datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
 def return_image(image_index, mnist_dataset):
@@ -93,4 +92,19 @@ def return_image(image_index, mnist_dataset):
     return image_matrix.reshape(image_matrix.size), image_matrix, label
 image_index = 19961
 x, image, label = return_image(image_index, get_mnist())
+
+
+# ---- BODY ----
+
+# Create and fill a network from the files
+the_network = network(n_vector)
+the_network.fill(weights,biases)
+
+# Do the input dance
+the_network.run(x)
+
+
+
+
+
 
